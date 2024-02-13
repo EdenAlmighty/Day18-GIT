75cf44f5bf661b918b59e39d1d37fe7aa157321b
EdenAlmighty <edengilady1@gmail.com>
2024-02-13 20:56:18 +0200

    Added third ball that toggles colors of ball1 & 2

--- js/game.js
+++ js/game.js
@@ -1,11 +1,28 @@
 'use strict'
 
+
+var ball1 = document.querySelector('.ball1')
+var ball2 = document.querySelector('.ball2')
+var ball3 = document.querySelector('.ball3')
+
+var elBalls = [ball1, ball2, ball3]
+
 function onBallClick(elBall, maxDiameter) {
-    var elBallSize = elBall.offsetWidth
-        elBallSize += getRandomInt(20, 60)
-        if (elBallSize >= maxDiameter) elBallSize = 100 
-        elBall.style.backgroundColor = getRandomColor()
-        elBall.style.height = elBallSize + 'px'
-        elBall.style.width = elBallSize + 'px'
-        elBall.innerHTML = elBallSize
-}
+    console.log(elBall);
+ 
+        if(elBall === elBalls[2]) {
+            var elBallColor = elBalls[1].style.backgroundColor
+            elBalls[1].style.backgroundColor = elBalls[0].style.backgroundColor
+            elBalls[0].style.backgroundColor = elBallColor
+        } else {
+            var elBallSize = elBall.offsetWidth
+            elBallSize += getRandomInt(20, 60)
+            
+            if (elBallSize >= maxDiameter) elBallSize = 100 
+    
+            elBall.style.backgroundColor = getRandomColor()
+            elBall.style.height = elBallSize + 'px'
+            elBall.style.width = elBallSize + 'px'
+            elBall.innerHTML = elBallSize
+        }
+    }
