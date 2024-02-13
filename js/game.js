5c5538b06bdd484cf6cfe0a3fa9a55492d7e02bb
EdenAlmighty <edengilady1@gmail.com>
2024-02-13 21:42:07 +0200

    Re-factor onBallClick() with onInit() and remove un-needed classes

--- js/game.js
+++ js/game.js
@@ -1,28 +1,29 @@
 'use strict'
 
+const gBalls = document.querySelectorAll('.ball')
+var elBalls
 
-var ball1 = document.querySelector('.ball1')
-var ball2 = document.querySelector('.ball2')
-var ball3 = document.querySelector('.ball3')
-
-var elBalls = [ball1, ball2, ball3]
+function onInit() {
+    elBalls = []
+    for (var i = 0; i < gBalls.length; i++) {
+        elBalls.push(gBalls[i])
+        elBalls[i].style.backgroundColor = getRandomColor()
+    }
+    return elBalls
+}
 
 function onBallClick(elBall, maxDiameter) {
-    console.log(elBall);
- 
-        if(elBall === elBalls[2]) {
-            var elBallColor = elBalls[1].style.backgroundColor
-            elBalls[1].style.backgroundColor = elBalls[0].style.backgroundColor
-            elBalls[0].style.backgroundColor = elBallColor
-        } else {
-            var elBallSize = elBall.offsetWidth
-            elBallSize += getRandomInt(20, 60)
-            
-            if (elBallSize >= maxDiameter) elBallSize = 100 
-    
-            elBall.style.backgroundColor = getRandomColor()
-            elBall.style.height = elBallSize + 'px'
-            elBall.style.width = elBallSize + 'px'
-            elBall.innerHTML = elBallSize
-        }
+    if (elBall === elBalls[2]) {
+        var elBallColor = elBalls[1].style.backgroundColor
+        elBalls[1].style.backgroundColor = elBalls[0].style.backgroundColor
+        elBalls[0].style.backgroundColor = elBallColor
+    } else {
+        var elBallSize = elBall.offsetWidth
+        elBallSize += getRandomInt(20, 60)
+        if (elBallSize >= maxDiameter) elBallSize = 100
+        elBall.style.backgroundColor = getRandomColor()
+        elBall.style.height = elBallSize + 'px'
+        elBall.style.width = elBallSize + 'px'
+        elBall.innerHTML = elBallSize
     }
+}
