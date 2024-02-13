2d070b02cdeaf131b111792781b75e5668416f3c
EdenAlmighty <edengilady1@gmail.com>
2024-02-13 15:17:20 +0200

    implement  onBallClick() and aligned text center

--- js/game.js
+++ js/game.js
@@ -1,5 +1,13 @@
 'use strict'
+var ball1Size = 100
+
 
 function onBallClick() {
-    console.log('clicked!');
+    var elBall = document.querySelector('.ball1')
+    ball1Size += 50 
+
+    elBall.style.height = ball1Size + 'px'
+    elBall.style.width = ball1Size  + 'px'
+    elBall.innerHTML = ball1Size
+
 }
