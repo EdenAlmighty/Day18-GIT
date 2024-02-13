6d20fde2953306f3747a914612ea37592efb2ac5
EdenAlmighty <edengilady1@gmail.com>
2024-02-13 17:20:03 +0200

    Implement getRandomInt() and resize ball on click

--- js/game.js
+++ js/game.js
@@ -4,7 +4,8 @@ var ball1Size = 100
 
 function onBallClick() {
     var elBall = document.querySelector('.ball1')
-    ball1Size += 50 
+    ball1Size += getRandomInt(20, 60) 
+    if (ball1Size >= 400) ball1Size = 100
 
     elBall.style.height = ball1Size + 'px'
     elBall.style.width = ball1Size  + 'px'
