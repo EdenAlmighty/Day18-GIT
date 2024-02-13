ab350fc008d4f65c39d02d2f2b052e3469837a24
EdenAlmighty <edengilady1@gmail.com>
2024-02-13 19:27:51 +0200

    First remote change

--- js/game.js
+++ js/game.js
@@ -1,11 +1,9 @@
 'use strict'
-// var ballSize = 100
 
 function onBallClick(elBall, maxDiameter) {
     var elBallSize = elBall.offsetWidth
         elBallSize += getRandomInt(20, 60)
-        if (elBallSize >= maxDiameter) elBallSize = 100
-    
+        if (elBallSize >= maxDiameter) elBallSize = 100 
         elBall.style.backgroundColor = getRandomColor()
         elBall.style.height = elBallSize + 'px'
         elBall.style.width = elBallSize + 'px'
